5ad9591842c992a3d05e989ceaefd08ab4484485
DavidVettuchirayil <DavidVettuchirayil@gmail.com>
2023-10-21 13:22:01 -0400

    Added ValidationExceptionHandler to ShowController

--- AniList/src/main/java/com/example/AniList/controller/ShowController.java
+++ AniList/src/main/java/com/example/AniList/controller/ShowController.java
@@ -148,4 +148,17 @@ public class ShowController {
             return ResponseEntity.notFound().build();
         }
     }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public Map<String, String> handleValidationExceptions(MethodArgumentNotValidException ex){
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+
+        return errors;
+    }
 }
