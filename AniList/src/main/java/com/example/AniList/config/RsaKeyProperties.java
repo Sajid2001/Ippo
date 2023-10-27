caf62da32de393f0304d4317631c74ede06004ec
Sajid2001 <60523377+Sajid2001@users.noreply.github.com>
2023-10-27 18:44:50 -0400

    user auth connected

--- AniList/src/main/java/com/example/AniList/config/RsaKeyProperties.java
+++
@@ -1,9 +0,0 @@
-package com.example.AniList.config;
-
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import java.security.interfaces.RSAPrivateKey;
-import java.security.interfaces.RSAPublicKey;
-
-@ConfigurationProperties(prefix = "rsa")
-public record RsaKeyProperties(RSAPublicKey publicKey, RSAPrivateKey privateKey) {
-}
